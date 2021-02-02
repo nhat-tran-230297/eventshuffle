5280acf54c0dc247da6cd3c9e84942fc9dcc8b0d
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-02-02 23:13:49 +0200

    update sqlite to postgres

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgres://rlpzzuuzbncnzp:46453c81fbe8f2b1d5a4baeda155237395b7399b8264b9895b107c04aaa7b58f@ec2-54-163-47-62.compute-1.amazonaws.com:5432/d1h7ptok5qvjmu'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
 
